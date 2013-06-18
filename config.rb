ffac9b0e4ae6f50ca7c30d17b7661a9743229f6b
Garrett Dawson <killtheliterate@gmail.com>
2013-06-18 00:13:50 -0600

    Initial commit

---
+++ config.rb
@@ -0,0 +1,9 @@
+require "susy"
+require "breakpoint"
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+output_stye = :compressed
+line_comments = false
